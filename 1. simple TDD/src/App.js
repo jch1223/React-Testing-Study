dc37a1da9e4e00ae19962841a91f819224389725
jch1223 <ico1828@gmail.com>
2022-04-09 23:13:30 +0900

    disabled 트리거 테스트

--- 1. simple TDD/src/App.js
+++ 1. simple TDD/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   return (
     <div className="App">
@@ -10,12 +11,17 @@ function App() {
         <h1 data-testid="count">{count}</h1>
 
         <div>
-          <button data-testid="plus-button" onClick={() => setCount(count + 1)}>
+          <button
+            data-testid="plus-button"
+            onClick={() => setCount(count + 1)}
+            disabled={isDisabled}
+          >
             +
           </button>
           <button
             data-testid="minus-button"
             onClick={() => setCount(count - 1)}
+            disabled={isDisabled}
           >
             -
           </button>
@@ -25,6 +31,7 @@ function App() {
           <button
             data-testid="on-off-button"
             style={{ backgroundColor: "red" }}
+            onClick={() => setIsDisabled(!isDisabled)}
           >
             on/off
           </button>
