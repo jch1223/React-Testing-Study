e544ee88ef346e01a9d78090f5b64e66e19550db
jch1223 <ico1828@gmail.com>
2022-04-09 23:00:04 +0900

    버튼 클릭 시 count 변경

--- 1. simple TDD/src/App.js
+++ 1. simple TDD/src/App.js
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 data-testid="count">0</h1>
+        <h1 data-testid="count">{count}</h1>
       </header>
 
       <div>
-        <button data-testid="plus-button">+</button>
-        <button data-testid="minus-button">-</button>
+        <button data-testid="plus-button" onClick={() => setCount(count + 1)}>
+          +
+        </button>
+        <button data-testid="minus-button" onClick={() => setCount(count - 1)}>
+          -
+        </button>
       </div>
     </div>
   );
