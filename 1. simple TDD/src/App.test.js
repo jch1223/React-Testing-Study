65dc804305bd69104e214ea989e1dbcca43d5355
jch1223 <ico1828@gmail.com>
2022-04-09 23:00:04 +0900

    버튼 클릭 시 count 변경

--- 1. simple TDD/src/App.test.js
+++ 1. simple TDD/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("카운터는 0부터 시작합니다.", () => {
@@ -17,4 +17,20 @@ test("플러스 버튼은 +를 가지고 있어야 합니다.", () => {
   render(<App />);
   const button = screen.getByTestId("plus-button");
   expect(button).toHaveTextContent("+");
+});
+
+test("마이너스 버튼을 눌렀을 때 count가 감소하여야 합니다.", () => {
+  render(<App />);
+  const button = screen.getByTestId("minus-button");
+  fireEvent.click(button);
+  const countElement = screen.getByTestId("count");
+  expect(countElement).toHaveTextContent(-1);
+});
+
+test("플러스 버튼을 눌렀을 때 count가 증가하여야 합니다.", () => {
+  render(<App />);
+  const button = screen.getByTestId("plus-button");
+  fireEvent.click(button);
+  const countElement = screen.getByTestId("count");
+  expect(countElement).toHaveTextContent(1);
 });
