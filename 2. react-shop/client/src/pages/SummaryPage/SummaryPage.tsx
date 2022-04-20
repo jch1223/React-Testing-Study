73c5d2abe9adfe8e333716512cd8bd967a928091
jch1223 <ico1828@gmail.com>
2022-04-20 23:42:30 +0900

    feat: summary page 렌더링

--- 2. react-shop/client/src/pages/SummaryPage/SummaryPage.tsx
+++ 2. react-shop/client/src/pages/SummaryPage/SummaryPage.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SummaryPage = () => {
-  return <div>SummaryPage</div>;
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <div>
+      <form>
+        <input
+          id="confirm-checkbox"
+          type="checkbox"
+          checked={checked}
+          onChange={(e) => setChecked(!e.target.checked)}
+        />
+        <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
+        <br />
+        <button type="submit" disabled={!checked}>
+          주문 확인
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SummaryPage;
