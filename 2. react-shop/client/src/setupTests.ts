4536c6a885f6c0821a24e97b59bcdc99403c870c
jch1223 <ico1828@gmail.com>
2022-04-21 00:12:38 +0900

    feat: msw 세팅

--- 2. react-shop/client/src/setupTests.ts
+++ 2. react-shop/client/src/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+import { server } from "./mocks/server";
+
+beforeAll(() => server.listen()); // 첫번째 테스트 실행 전에 한번 실행
+afterEach(() => server.resetHandlers()); // 각각의 테스트가 끝나면 실행
+afterAll(() => server.close()); // 마지막 테스트가 끝나면 샐행
