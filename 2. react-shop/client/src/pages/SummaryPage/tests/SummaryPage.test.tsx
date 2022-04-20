c68e0d26cec80683e077317b95e95328b0a0df49
jch1223 <ico1828@gmail.com>
2022-04-20 23:33:10 +0900

    test: summary page test

---
+++ 2. react-shop/client/src/pages/SummaryPage/tests/SummaryPage.test.tsx
@@ -0,0 +1,15 @@
+import SummaryPage from "../SummaryPage";
+import { render, screen } from "@testing-library/react";
+
+test("체크박스와 버튼이 렌더링 되어야 합니다.", () => {
+  render(<SummaryPage />);
+  const checkbox: HTMLInputElement = screen.getByRole("checkbox", {
+    name: "주문하려는 것을 확인하셨나요?",
+  });
+  expect(checkbox.checked).toEqual(false);
+
+  const confirmButton: HTMLButtonElement = screen.getByRole("button", {
+    name: "주문 확인",
+  });
+  expect(confirmButton).toBeDisabled();
+});
