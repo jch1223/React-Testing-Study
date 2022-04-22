6bcfe06595ecd92560f6844aac0b8a3f11bb1aba
jch1223 <ico1828@gmail.com>
2022-04-23 00:39:30 +0900

    test: error handling 테스트 작성

--- 2. react-shop/client/src/components/Order/tests/Type.test.tsx
+++ 2. react-shop/client/src/components/Order/tests/Type.test.tsx
@@ -1,4 +1,7 @@
 import { render, screen } from "@testing-library/react";
+import { rest } from "msw";
+
+import { server } from "../../../mocks/server";
 
 import Type from "../Type";
 
@@ -13,4 +16,17 @@ test("이미지가 렌더링 되어야 합니다.", async () => {
 
   const altTexts = productImages.map((element) => element.alt);
   expect(altTexts).toEqual(["America product", "England product"]);
+});
+
+test("데이터를 가져오는데에 실패하면 에러 배너가 나타나야 합니다.", async () => {
+  server.resetHandlers(
+    rest.get("http://localhost:5000/products", (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
+
+  render(<Type orderType="products" />);
+
+  const errorBanner = await screen.findByTestId("error-banner");
+  expect(errorBanner).toHaveTextContent("에러가 발생했습니다.");
 });
