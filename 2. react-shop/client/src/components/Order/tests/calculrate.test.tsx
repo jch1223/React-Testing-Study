5ad8557bdbf928f3182017c8e9fa87b66d8d46ce
jch1223 <ico1828@gmail.com>
2022-06-05 15:54:49 +0900

    test: calculrate test 작성

---
+++ 2. react-shop/client/src/components/Order/tests/calculrate.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Type from "../Type";
+
+test("프로덕트가 변경되면 토탈 가격이 변경되어야 합니다.", async () => {
+  render(<Type orderType="products" />);
+
+  const productsTotal = screen.getByText("상품 총 가격:", { exact: false });
+  expect(productsTotal).toHaveTextContent("0");
+
+  const americaInput = await screen.findByRole("spinbutton", {
+    name: "America",
+  });
+
+  userEvent.clear(americaInput);
+  userEvent.type(americaInput, "1");
+  expect(productsTotal).toHaveTextContent("1000");
+});
