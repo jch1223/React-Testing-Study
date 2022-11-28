d7ac56eaa85fc6d7931d5125c787e59f17e588d9
jch1223 <ico1828@gmail.com>
2022-11-28 23:08:53 +0900

    test: 총가격 테스트 케이스 작성

--- 2. react-shop/client/src/components/Order/tests/calculrate.test.tsx
+++ 2. react-shop/client/src/components/Order/tests/calculrate.test.tsx
@@ -1,5 +1,6 @@
 import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import OrderPage from "../../../pages/OrderPage/OrderPage";
 import { render } from "../../../test-utils";
 import Type from "../Type";
 
@@ -38,4 +39,59 @@ test("옵션이 변경되면 옵션 총 가격이 변경되어야 합니다.", a
 
   userEvent.click(dinnerCheckbox);
   expect(optionsTotal).toHaveTextContent("500");
+});
+
+describe("옵션과 상품 총 가격 계산", () => {
+  test("총 가격은 0원에서 시작하고, 상품과 옵션 가격이 더해져야 합니다", async () => {
+    render(<OrderPage />);
+
+    const total = screen.getByText("Total Price:", { exact: false });
+    expect(total).toHaveTextContent("0");
+
+    const americaInput = await screen.findByRole("spinbutton", {
+      name: "America",
+    });
+
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, "1");
+    expect(total).toHaveTextContent("1000");
+
+    const insuranceCheckbox = await screen.findByRole("checkbox", {
+      name: "Insurance",
+    });
+
+    userEvent.click(insuranceCheckbox);
+    expect(total).toHaveTextContent("1500");
+  });
+
+  test("옵션이 추가 될 때 가격이 수정되어야 합니다", async () => {
+    const total = screen.getByText("Total Price:", { exact: false });
+    const insuranceCheckbox = await screen.findByRole("checkbox", {
+      name: "Insurance",
+    });
+
+    userEvent.click(insuranceCheckbox);
+    expect(total).toHaveTextContent("500");
+  });
+
+  test("상품과 옵션이 제거 되었을 때 가격이 수정되어야 합니다", async () => {
+    render(<OrderPage />);
+
+    const total = screen.getByText("Total Price:", { exact: false });
+    const americaInput = await screen.findByRole("spinbutton", {
+      name: "America",
+    });
+
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, "3");
+    userEvent.type(americaInput, "1");
+
+    const insuranceCheckbox = await screen.findByRole("checkbox", {
+      name: "Insurance",
+    });
+
+    userEvent.click(insuranceCheckbox);
+    userEvent.click(insuranceCheckbox);
+    expect(total).toHaveTextContent("1000");
+  });
 });
