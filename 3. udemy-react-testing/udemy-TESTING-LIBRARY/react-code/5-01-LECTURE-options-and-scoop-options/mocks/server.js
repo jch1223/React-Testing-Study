d6f2fab4682b44ba94c5c2535cc0ff0c3bc28814
jch1223 <ico1828@gmail.com>
2023-02-01 15:47:26 +0900

    test: 동일한 endpoint에서 에러 응답을 테스트 하는 예제

--- 3. udemy-react-testing/udemy-TESTING-LIBRARY/react-code/5-01-LECTURE-options-and-scoop-options/mocks/server.js
+++ 3. udemy-react-testing/udemy-TESTING-LIBRARY/react-code/5-01-LECTURE-options-and-scoop-options/mocks/server.js
@@ -1,5 +1,38 @@
-import { setupServer } from 'msw/node';
-import { handlers } from './handlers';
+import { setupServer } from "msw/node";
+import { handlers } from "./handlers";
 
 // This configures a request mocking server with the given request handlers.
 export const server = setupServer(...handlers);
+
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const serverExample = setupServer(
+  // I recommend listing the success scenarios when declaring the server.
+  rest.get("/resource", (req, res, ctx) => {
+    return res(ctx.json({ id: 1 }));
+  })
+);
+
+beforeAll(() => serverExample.listen());
+afterEach(() => serverExample.resetHandlers());
+afterAll(() => serverExample.close());
+
+it("handles a successful response", () => {
+  // run your code (i.e. render components, execute functions)
+  // without any additions, this test case will receive a successful response
+  // defined above.
+});
+
+it("handles an error response", () => {
+  serverExample.use(
+    // In this paticular test respond to the same request with a 404 response.
+    rest.get("/resource", (req, res, ctx) => {
+      return res(ctx.status(404));
+    })
+  );
+
+  // run your code here.
+  // within this test a "GET /resource" will receive a 404 error response
+  // specified above.
+});
